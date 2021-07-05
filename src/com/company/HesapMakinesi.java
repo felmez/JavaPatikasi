bb4863430538920b87cd91d7e07b8f62f1d7e05e
idk <idk@idks-MacBook-Pro.local>
2021-07-05 23:23:48 +0300

    Add hesap makinesi

---
+++ src/com/company/HesapMakinesi.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.*;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        double first, second, result = 0;
+        int selection;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ilk Sayiyi Giriniz: ");
+        first = input.nextDouble();
+        System.out.print("Ikinci Sayiyi Giriniz: ");
+        second = input.nextDouble();
+        System.out.println("Lutfen Islemi Seciniz: \n1-+\n2--\n3-*\n4-/");
+        selection = input.nextInt();
+        switch (selection) {
+            case 1:
+                result = first + second;
+                System.out.println(result);
+                break;
+            case 2:
+                result = first - second;
+                System.out.println(result);
+                break;
+            case 3:
+                result = first * second;
+                System.out.println(result);
+                break;
+            case 4:
+                result = first / second;
+                System.out.println(result);
+                break;
+            default:
+                System.out.println("Hatali Secim veya Giris");
+                break;
+        }
+    }
+}
