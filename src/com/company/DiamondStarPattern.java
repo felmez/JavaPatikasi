b7101fcea89b6928bd06aae32824e04197e9fecf
Souhaib <sofelmez@gmail.com>
2021-07-14 19:02:39 +0300

    Add diamond pattern printer

---
+++ src/com/company/DiamondStarPattern.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DiamondStarPattern {
+    public static void main(String[] args) {
+        int numberOfLines;
+        String symbol;
+        char space = ' ';
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter The Number of Lines: ");
+        numberOfLines = input.nextInt();
+
+        System.out.print("Enter The Symbol to Print: ");
+        symbol = input.next();
+
+        for (int i = 0; i <= numberOfLines; i++) {
+            for (int j = 0; j < (numberOfLines - i); j++) {
+                System.out.print(space);
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+
+        for (int i = numberOfLines - 1; i > 0; i--) {
+            for (int j = 1; j <= (numberOfLines - i); j++) {
+                System.out.print(space);
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+    }
+}
