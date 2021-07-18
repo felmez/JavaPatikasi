193fcb74286f08d9a4afb3381f7e8982ff7e1ce8
Souhaib <sofelmez@gmail.com>
2021-07-18 13:21:33 +0300

    Add palindrome number checker

---
+++ src/com/company/Palindrome.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    static boolean isPalindrome(int i) {
+        int tempNumber = i, reversedNumber = 0, lastDigit;
+
+        while (tempNumber != 0) {
+            lastDigit = tempNumber % 10;
+            reversedNumber = (reversedNumber * 10) + lastDigit;
+            tempNumber /= 10;
+        }
+        return i == reversedNumber;
+    }
+
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a Palindrome.");
+        } else {
+            System.out.println(number + " is not a Palindrome.");
+        }
+    }
+}
