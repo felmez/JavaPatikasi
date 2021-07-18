d51d0dcd9729f1948e7ff6b878e807ee267af7d9
Souhaib <sofelmez@gmail.com>
2021-07-18 23:46:51 +0300

    Edit digits summation

--- src/com/company/DigitsSummation.java
+++ src/com/company/DigitsSummation.java
@@ -2,11 +2,11 @@ package com.company;
 
 import java.util.Scanner;
 
-public class BasamakToplami {
+public class DigitsSummation {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Bir Sayi Giriniz: ");
+        System.out.print("Enter a number: ");
 
         int inputNumber = input.nextInt(),
                 digitValue, digitsTotalValue = 0,
@@ -19,7 +19,7 @@ public class BasamakToplami {
             inputNumber /= 10;
         }
 
-        System.out.println("Basamak Sayisi: " + digitCounter + "\nBasamak Sayilarin Toplami: " + digitsTotalValue);
+        System.out.println("Digits Number: " + digitCounter + "\nDigits Summation: " + digitsTotalValue);
 
     }
 }
