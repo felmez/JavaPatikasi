828e37967dcca65442e93e2689f5851b4d4416e4
Souhaib <sofelmez@gmail.com>
2021-07-18 23:50:19 +0300

    Edit summation of multiples of four

--- src/com/company/SummationOfMultiplesOfFour.java
+++ src/com/company/SummationOfMultiplesOfFour.java
@@ -2,20 +2,20 @@ package com.company;
 
 import java.util.Scanner;
 
-public class DortKatiSayilarinToplami {
+public class SummationOfMultiplesOfFour {
     public static void main(String[] args) {
         int number, total = 0;
 
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.print("Bir Sayi Giriniz: ");
+            System.out.print("Enter a number: ");
             number = input.nextInt();
             if (number % 4 == 0) {
                 total += number;
             }
         } while (number % 2 == 0);
 
-        System.out.println("Girilen Cift Sayilarin Toplami: " + total);
+        System.out.println("Even numbers summation is: " + total);
     }
 }
