05bed2cae19e191818a3c7f67aea7af23b347ac6
Souhaib <sofelmez@gmail.com>
2021-07-19 00:08:53 +0300

    Edit harmonic series

--- src/com/company/HarmonicSeries.java
+++ src/com/company/HarmonicSeries.java
@@ -1,20 +1,20 @@
 package com.company;
 import java.util.Scanner;
 
-public class HarmonikSeri {
+public class HarmonicSeries {
     public static void main(String[] args) {
         int number;
         double result = 0;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Bir Sayi Giriniz: ");
+        System.out.print("Enter a number: ");
         number = input.nextInt();
 
         for (double i = 1.0; i <= number; i++) {
             result += 1/i;
         }
 
-        System.out.println("Sonuc: " + String.format("%.2f", result));
+        System.out.println("Result: " + String.format("%.2f", result));
     }
 }
