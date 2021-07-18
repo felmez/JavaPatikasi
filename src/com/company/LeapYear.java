24bde9f7c1db45cca87c19d918156888568da9f1
Souhaib <sofelmez@gmail.com>
2021-07-18 23:45:14 +0300

    Edit leap year

--- src/com/company/LeapYear.java
+++ src/com/company/LeapYear.java
@@ -1,20 +1,20 @@
 package com.company;
 import java.util.Scanner;
 
-public class ArtikYil {
+public class LeapYear {
     public static void main(String[] args) {
         int year;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Yil Giriniz: ");
+        System.out.print("Enter Year: ");
         year = input.nextInt();
 
         if (year % 4 == 0 || year % 400 == 0) {
-            System.out.println(year + " bir artik yildir!");
+            System.out.println(year + " is a leap year.");
         }
         else {
-            System.out.println(year + " bir artik yil degildir!");
+            System.out.println(year + " is not a leap year.");
         }
     }
 }
