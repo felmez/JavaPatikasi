7c82f03bed52a021d6ac0ba888a4acaa2d4c487e
Souhaib <sofelmez@gmail.com>
2021-07-19 00:03:19 +0300

    Edit circle area

--- src/com/company/CircleArea.java
+++ src/com/company/CircleArea.java
@@ -2,22 +2,26 @@ package com.company;
 import java.util.*;
 
 
-public class DaireAlani {
+public class CircleArea {
     public static void main(String[] args) {
         final double PI = 3.14;
         int radius;
         double area;
         double circumference;
         double arc;
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Dairenin Yaricapini Giriniz: ");
+
+        System.out.print("Enter the circle radius: ");
         radius = input.nextInt();
+
         area = PI * Math.pow(radius, 2);
         circumference = PI * radius * 2;
         arc = (PI * Math.pow(radius, 2) * area) / 360;
-        System.out.println("Dairenin Alani: " + area);
-        System.out.println("Dairenin Cevresi: " + String.format("%.2f", circumference));
-        System.out.println("Yay Alani: " + String.format("%.2f", arc));
+
+        System.out.println("Circle Area is: " + area);
+        System.out.println("Circle Circumference is: " + String.format("%.2f", circumference));
+        System.out.println("Circle Arc Length is: " + String.format("%.2f", arc));
 
     }
 }
