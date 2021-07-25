9e92022d406b4bc7ea217812720e00a6db93d1a6
Souhaib <sofelmez@gmail.com>
2021-07-25 23:43:33 +0300

    Add array harmonic mean

---
+++ src/com/company/ArrayHarmonicMean.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class ArrayHarmonicMean {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double harmonicSeries = 0;
+        double harmonicMean;
+
+        for (int i = 0; i < numbers.length; i++) {
+            harmonicSeries += 1 / numbers[i];
+        }
+
+        harmonicMean = numbers.length / harmonicSeries;
+
+        System.out.println("Array items harmonic mean is " + String.format("%.2f", harmonicMean));
+        System.out.println("Array items harmonic series is " + String.format("%.2f", harmonicSeries));
+    }
+}
