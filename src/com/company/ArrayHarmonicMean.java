9fd62eee1436795a715f061ebb5522721c3c426e
Souhaib <sofelmez@gmail.com>
2021-07-25 23:44:16 +0300

    Edit array harmonic mean

--- src/com/company/ArrayHarmonicMean.java
+++ src/com/company/ArrayHarmonicMean.java
@@ -3,7 +3,7 @@ package com.company;
 public class ArrayHarmonicMean {
     public static void main(String[] args) {
         double[] numbers = {1, 2, 3, 4, 5};
-        double harmonicSeries = 0;
+        double harmonicSeries = 0.0;
         double harmonicMean;
 
         for (int i = 0; i < numbers.length; i++) {
